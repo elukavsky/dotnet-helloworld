6dbe55a6d50102ec540a1687537358229ea1693f
elukavsky <elukavsky@anexinet.com>
2018-11-09 11:01:03 -0500

    0204-BooleanDataType

---
+++ 0204-BooleanDataType/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // booleans
+        bool isFull = false;
+
+        bool isFourBiggerThanSix = 4 > 6;
+
+        Console.WriteLine(isFourBiggerThanSix);
+    }
+}
