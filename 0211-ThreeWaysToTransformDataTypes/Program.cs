6f8c333868850d012f25a8d1d1cbe7e7df5de1cb
elukavsky <elukavsky@anexinet.com>
2018-11-09 13:39:51 -0500

    0211-ThreeWaysToTransformDataTypes

---
+++ 0211-ThreeWaysToTransformDataTypes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int number = 5;
+        string word = "five";
+        char x = 'x';
+        float pi = 3.14f;
+
+        float integer = number; // You can save int as a decimal
+        // int floatInt = pi;  // Can't save decimal value as int
+        int floatInt = (int)pi; // Need to convert decimal to int (but decimal is rounded to a wholoe number)
+
+        Console.WriteLine(floatInt);
+
+        // int stringInt = word; // a string is not an int
+        //int stringInt = Convert.ToInt32(word); // will not work because the string "five" is not the same as the string "5"
+        word = "5"; // string has a numeric equivalent
+        int stringInt = Convert.ToInt32(word);
+        Console.WriteLine(stringInt); 
+    }
+}
