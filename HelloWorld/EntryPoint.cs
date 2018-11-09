a4ea7e8c3e9b47743e2b7210fa7ba3bf2e7cc060
elukavsky <elukavsky@anexinet.com>
2018-11-09 10:20:48 -0500

    Primitive Data Types and Variables

--- HelloWorld/EntryPoint.cs
+++ HelloWorld/EntryPoint.cs
@@ -5,5 +5,16 @@ class EntryPoint
     static void Main()
     {
         Console.WriteLine("Hello World");
+
+        int age = 36;
+
+        Console.WriteLine(age);
+
+        string firstName = "Eric";
+
+        Console.WriteLine(firstName);
+
+        // Combine variables in one line
+        Console.WriteLine("Age: " + age + " Name: " + firstName);
     }
 }
