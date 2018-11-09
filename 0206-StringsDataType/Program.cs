f685766dc6f8af3c4975545d739a5982f09e3668
elukavsky <elukavsky@anexinet.com>
2018-11-09 11:14:32 -0500

    0206-StringsDataType

---
+++ 0206-StringsDataType/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // strings are immutable
+        string username = "admin";
+
+        Console.WriteLine(username[0]);
+        Console.WriteLine(username[username.Length - 1]);
+    }
+}
