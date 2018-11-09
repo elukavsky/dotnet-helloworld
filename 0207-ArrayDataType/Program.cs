67b176f072abd8d87b69258218c3ce8ca3c029e9
elukavsky <elukavsky@anexinet.com>
2018-11-09 11:29:06 -0500

    0207-ArrayDataType

---
+++ 0207-ArrayDataType/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Arrays
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        Console.WriteLine(numbers[3]);
+
+        // All values in array are 0
+        int[] numbers2 = new int[5];
+
+        Console.WriteLine(numbers2[3]);
+
+        string[] fruits = { "apple", "orange", "pear", "plum", "peach" };
+        fruits[3] = "strawberry";
+
+        Console.WriteLine(fruits[3][5]);
+    }
+}
