8effb5437d4564dd1bb98c8cba175a86fd08ee84
elukavsky <elukavsky@anexinet.com>
2018-11-09 11:08:46 -0500

    0205-SingleCharacterDataType

---
+++ 0205-SingleCharacterDataType/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        // If special characters don't work
+        Console.InputEncoding = Encoding.UTF8;
+        Console.OutputEncoding = Encoding.UTF8;
+
+        // Single character types (Must use single quotation marks)
+        char theCharacterX = 'x';
+
+        Console.WriteLine(theCharacterX);
+
+        // ASCII
+        char thePlusSign = '\u002B';
+
+        Console.WriteLine(thePlusSign);
+
+        // Unicode
+        char unicodeSymbol = '\u00f6';
+
+        Console.WriteLine(unicodeSymbol);
+    }
+}
