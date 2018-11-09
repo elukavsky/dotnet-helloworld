28b51bce39c85a80e552f141d5a516c691b8fd4d
elukavsky <elukavsky@anexinet.com>
2018-11-09 13:20:23 -0500

    0209-TransformingDataTypes

---
+++ 0209-TransformingDataTypes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Convert int to string
+        int bigNumber = 3423432;
+
+        Console.WriteLine(bigNumber.ToString().Contains("2"));
+
+        // Combining strings
+        string iLearn = "I am learning ";
+        string course = "C# Mastercourse";
+
+        string concatenated = iLearn + course;
+
+        Console.WriteLine(concatenated);
+    }
+}
